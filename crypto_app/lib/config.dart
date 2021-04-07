fac91b71cc84f4e5e2e8a69f64926a005320f78c
KwekuYamoah <kwekuandyamoah@gmail.com>
2021-04-07 14:54:00 +0000

    Alby API key added. Twitter next

---
+++ crypto_app/lib/config.dart
@@ -0,0 +1,10 @@
+/// API keys for API calls
+
+const String APIKey = 'uxFbuQ.6-luPQ:rXpw5aF4I14ZkcD8';
+
+/// Twitter auths will come later
+const String OAuthConsumerKey = 'Place your twitter oauth_consumer_key here';
+const String OAuthConsumerSecret =
+    'Place your twitter oauth_consumer_secret here';
+const String OAuthToken = 'Place your twitter oauth_token here';
+const String OAuthTokenSecret = 'Place your twitter oauth_token_secret here';
